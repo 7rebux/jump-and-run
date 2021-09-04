c06f733696fec588529e440d49d1adabf2db8c38
7Rebux <imrebux@gmail.com>
2021-09-05 00:41:11 +0200

    Now runs asynchronously

--- src/main/kotlin/net/rebux/jumpandrun/inventories/ParkourInventory.kt
+++ src/main/kotlin/net/rebux/jumpandrun/inventories/ParkourInventory.kt
@@ -3,15 +3,18 @@ package net.rebux.jumpandrun.inventories
 import net.rebux.jumpandrun.Main
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
 
 class ParkourInventory(private val player: Player?) {
-    val inventory = Bukkit.createInventory(null, 36, "Parcours");
+    val inventory: Inventory = Bukkit.createInventory(null, 36, "Parcours");
 
     init {
         if (player != null) loadContents()
     }
 
     private fun loadContents() {
-        Main.instance.parkourManager.parkours.forEach { inventory.addItem(it.getItem(player!!)) }
+        Bukkit.getScheduler().runTaskAsynchronously(Main.instance) {
+            Main.instance.parkourManager.parkours.forEach { inventory.addItem(it.getItem(player!!)) }
+        }
     }
 }
