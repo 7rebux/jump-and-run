4fc912d0a7e326fc84e30c7dca1f7f9405439951
7rebux <imrebux@gmail.com>
2022-10-21 00:02:06 +0200

    Use shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,57 @@
 @file:Suppress("SpellCheckingInspection")
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URI
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
+val pluginName = "JumpAndRun"
 val exposedVersion: String by project
 
 plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("jvm") version "1.7.20"
 }
 
+group = "net.rebux.jumpandrun"
+version = "2.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
 repositories {
     mavenCentral()
-
-    maven {
-        url = URI("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-
-        content {
-            includeGroup("org.bukkit")
-            includeGroup("org.spigotmc")
-        }
-    }
-
-    maven {
-        url = URI("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
-    implementation("org.bukkit:bukkit:1.8.8-R0.1-SNAPSHOT")
-    implementation("com.github.azbh111:craftbukkit-1.8.8:R")
+    compileOnly("org.bukkit:bukkit:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("com.github.azbh111:craftbukkit-1.8.8:R")
 
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
-
     implementation("org.slf4j:slf4j-nop:2.0.3")
 
-    testImplementation(kotlin("test"))
+    shadow("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.named<ShadowJar>("shadowJar") {
+    manifest {
+        attributes(mapOf("Main-Class" to "$group/Plugin"))
+    }
+
+    archiveBaseName.set(pluginName)
+    archiveClassifier.set("")
+    archiveVersion.set("")
+    destinationDir = File("./_server/plugins")
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
