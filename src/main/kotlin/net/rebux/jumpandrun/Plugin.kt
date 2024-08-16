6786fe8992ff1636a6856a2920dcc371d1cc7289
Nils Osswald <contact@nilsosswald.de>
2024-08-16 09:29:49 +0200

    Reformat

--- src/main/kotlin/net/rebux/jumpandrun/Plugin.kt
+++ src/main/kotlin/net/rebux/jumpandrun/Plugin.kt
@@ -8,9 +8,7 @@ import net.rebux.jumpandrun.commands.TopCommand
 import net.rebux.jumpandrun.database.DatabaseConnector
 import net.rebux.jumpandrun.database.SchemaInitializer
 import net.rebux.jumpandrun.listeners.*
-import net.rebux.jumpandrun.parkour.ParkourDifficulty
 import net.rebux.jumpandrun.parkour.ParkourManager
-import org.bukkit.Material
 import org.bukkit.command.CommandExecutor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
@@ -53,14 +51,6 @@ class Plugin : JavaPlugin() {
             "practice" to PracticeCommand(),
             "top" to TopCommand())
     }
-
-    // TODO: Remove
-    private val materialByDifficulty =
-        mapOf(
-            ParkourDifficulty.EASY to Material.GREEN_SHULKER_BOX,
-            ParkourDifficulty.NORMAL to Material.YELLOW_SHULKER_BOX,
-            ParkourDifficulty.HARD to Material.RED_SHULKER_BOX,
-            ParkourDifficulty.ULTRA to Material.PURPLE_SHULKER_BOX)
 
     private fun registerListeners(vararg listener: Listener) {
         listener.forEach { server.pluginManager.registerEvents(it, this) }
--- src/test/kotlin/MessageBuilderTests.kt
+++ src/test/kotlin/MessageBuilderTests.kt
@@ -4,76 +4,76 @@ import kotlin.test.assertEquals
 
 internal class MessageBuilderTests {
 
-  @Test
-  fun buildsSimpleMessage() {
-    val expected = "message";
-    assertEquals(
-      expected,
-      MessageBuilder("message")
-        .prefix(false)
-        .buildSingle()
-    )
-  }
+    @Test
+    fun buildsSimpleMessage() {
+        val expected = "message";
+        assertEquals(
+            expected,
+            MessageBuilder("message")
+                .prefix(false)
+                .buildSingle()
+        )
+    }
 
-  @Test
-  fun buildsMultilineMessage() {
-    val expected = listOf("line1", "line2")
-    assertEquals(
-      expected,
-      MessageBuilder("line1\nline2")
-        .prefix(false)
-        .build()
-    )
-  }
+    @Test
+    fun buildsMultilineMessage() {
+        val expected = listOf("line1", "line2")
+        assertEquals(
+            expected,
+            MessageBuilder("line1\nline2")
+                .prefix(false)
+                .build()
+        )
+    }
 
-  @Test
-  fun buildsSimpleErrorMessage() {
-    val expected = "§4error"
-    assertEquals(
-      expected,
-      MessageBuilder("error")
-        .error()
-        .prefix(false)
-        .buildSingle()
-    )
-  }
+    @Test
+    fun buildsSimpleErrorMessage() {
+        val expected = "§4error"
+        assertEquals(
+            expected,
+            MessageBuilder("error")
+                .error()
+                .prefix(false)
+                .buildSingle()
+        )
+    }
 
-  @Test
-  fun buildsMultilineErrorMessage() {
-    val expected = listOf("§4error1", "§4error2")
-    assertEquals(
-      expected,
-      MessageBuilder("error1\nerror2")
-        .error()
-        .prefix(false)
-        .build()
-    )
-  }
+    @Test
+    fun buildsMultilineErrorMessage() {
+        val expected = listOf("§4error1", "§4error2")
+        assertEquals(
+            expected,
+            MessageBuilder("error1\nerror2")
+                .error()
+                .prefix(false)
+                .build()
+        )
+    }
 
-  @Test
-  fun insertsTemplateValue() {
-    val expected = "value=2"
-    assertEquals(
-      expected,
-      MessageBuilder("value={value}")
-        .values(mapOf("value" to 2))
-        .prefix(false)
-        .buildSingle()
-    )
-  }
+    @Test
+    fun insertsTemplateValue() {
+        val expected = "value=2"
+        assertEquals(
+            expected,
+            MessageBuilder("value={value}")
+                .values(mapOf("value" to 2))
+                .prefix(false)
+                .buildSingle()
+        )
+    }
 
-  @Test
-  fun buildsComplexMessage() {
-    val expected = listOf("§4line1=1", "§4line2=2")
-    assertEquals(
-      expected,
-      MessageBuilder("line1={line1}\nline2={line2}")
-        .values(mapOf(
-          "line1" to 1,
-          "line2" to 2))
-        .error()
-        .prefix(false)
-        .build()
-    )
-  }
+    @Test
+    fun buildsComplexMessage() {
+        val expected = listOf("§4line1=1", "§4line2=2")
+        assertEquals(
+            expected,
+            MessageBuilder("line1={line1}\nline2={line2}")
+                .values(mapOf(
+                    "line1" to 1,
+                    "line2" to 2))
+                .error()
+                .prefix(false)
+                .build()
+        )
+    }
 }
